e9713a929d7b4b16b7c7b7fcb440db176f209d17
Marta Costa <martapcosta@gmail.com>
2017-04-25 22:36:31 +0200

    ListPanelCtrl (for  issues) Implementation of logout  controller

--- js/app.js
+++ js/app.js
@@ -9,6 +9,53 @@ angular.module('app', [
 ]);
 
 /**
+ * Issues list controller
+ */
+angular.module('app').controller('ListPanelController', function(DataService) {
+
+    var listPanelCtrl = this;
+
+    //listPanelCtrl.user = "Ken Bogard";
+
+    DataService.getIssues().then(function(descriptions) {
+        listPanelCtrl.descriptions = descriptions;
+    });
+
+});
+
+angular.module('app').factory('DataService', function($q) {
+
+    var service = {};
+
+    service.getIssues = function() {
+        return $q.when([
+        {
+            title: 'Head 1',
+            text: 'Problem 1 blabla',
+            time: moment().hour(12).minute(40).toDate(),
+            ncomments: 2
+        },
+        {
+            title: 'Head 2',
+            text: 'Problem 2 blablaHey\nWhat\'s up?',
+            time: moment().hour(12).minute(50).toDate(),
+            ncomments: 23
+        },
+        {
+            title: 'Headsss 3',
+            text: 'Problem 3 Same old, same old.\nWanna come\'n play some SFV?!',
+            time: moment().year(2017).day(25).month(4).hour(14).minute(50).toDate(),
+            ncomments: 44
+        }
+        ]);
+    };
+
+    return service;
+});
+
+
+
+/**
  * Config function with the navigation states
  */
 angular.module('app').config(function ($stateProvider, $urlRouterProvider, $httpProvider) {
